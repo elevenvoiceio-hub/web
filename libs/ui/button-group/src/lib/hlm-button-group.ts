9bc03554e05b539195e3d6cdb05b255b945b62d2
mohitmadras <mohitmadras@gmail.com>
2025-12-27 21:30:28 +0530

    update changes to login page

--- libs/ui/button-group/src/lib/hlm-button-group.ts
+++ libs/ui/button-group/src/lib/hlm-button-group.ts
@@ -4,7 +4,7 @@ import { cva } from 'class-variance-authority';
 import { ClassValue } from 'clsx';
 
 export const buttonGroupVariants = cva(
-	"flex w-fit items-stretch has-[>[data-slot=button-group]]:gap-2 [&>*]:focus-visible:relative [&>*]:focus-visible:z-10 has-[select[aria-hidden=true]:last-child]:[&>[data-slot=select-trigger]:last-of-type]:rounded-r-md [&>[data-slot=select-trigger]:not([class*='w-'])]:w-fit [&>input]:flex-1",
+	"cursor-pointer flex w-fit items-stretch has-[>[data-slot=button-group]]:gap-2 [&>*]:focus-visible:relative [&>*]:focus-visible:z-10 has-[select[aria-hidden=true]:last-child]:[&>[data-slot=select-trigger]:last-of-type]:rounded-r-md [&>[data-slot=select-trigger]:not([class*='w-'])]:w-fit [&>input]:flex-1",
 	{
 		variants: {
 			orientation: {
--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,23 +1,33 @@
 import { CommonService } from '../../../services/common-service/common-service';
-import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideAudioLines, lucideEye, lucideEyeOff, lucideLock, lucideMail } from '@ng-icons/lucide';
+import {
+  lucideAudioLines,
+  lucideEye,
+  lucideEyeOff,
+  lucideLock,
+  lucideMail,
+} from '@ng-icons/lucide';
 import { HlmButton } from '@spartan-ng/helm/button';
 import { HlmInput } from '@spartan-ng/helm/input';
 import { UserService } from '../../../services/user/user-service';
 import { LocalStorageService } from '../../../services/local-storage-service/local-storage-service';
 import { environment } from '../../../../environments/environment';
 import { HlmInputGroupImports } from '@spartan-ng/helm/input-group';
+import { HlmSpinnerImports } from '@spartan-ng/helm/spinner';
 
 @Component({
-  imports: [NgIcon, RouterModule, HlmButton, ReactiveFormsModule, HlmInput, HlmInputGroupImports],
+  imports: [
+    NgIcon,
+    RouterModule,
+    HlmButton,
+    ReactiveFormsModule,
+    HlmInput,
+    HlmInputGroupImports,
+    HlmSpinnerImports,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
   providers: [
@@ -28,14 +38,13 @@ import { HlmInputGroupImports } from '@spartan-ng/helm/input-group';
 export class Login {
   applicationName = environment.applicationName;
   welcomeWebsiteURL = environment.welcomeWebsite;
-  showPassword = false;
+  showPassword = signal<boolean>(false);
+  loading = signal<boolean>(false);
+  errorMessage = signal<string>('');
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(6),
-    ]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
   });
 
   constructor(
@@ -52,16 +61,23 @@ export class Login {
       login: this.loginForm.value.email!,
       password: this.loginForm.value.password!,
     };
+    this.errorMessage.set('');
     this.userService.Login(requestBody).subscribe({
       next: (data) => {
+        this.loading.set(true);
+        this.loginForm.disable();
         const userData = { ...data, email: this.loginForm.value.email! };
         this.localStorageService.saveData('user', JSON.stringify(userData));
         this.route.navigate(['/app']);
       },
-      error: () => {
-        this.commonService.setToaster(
-          'Failed to Login. Please try after sometime'
-        );
+      error: (e) => {
+        this.loading.set(false);
+        this.loginForm.enable();
+        if (e?.error?.detail.non_field_errors[0] === 'Invalid login credentials.') {
+          this.errorMessage.set('Invalid email or password');
+        } else {
+          this.commonService.setToaster('Failed to Login. Please try after sometime');
+        }
       },
     });
   };
