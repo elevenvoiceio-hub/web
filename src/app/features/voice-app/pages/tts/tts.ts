ec488bde893e2907962b13a0d1ae55f6a69af4cb
mohitmadras <mohitmadras@gmail.com>
2026-01-01 00:44:35 +0530

    updated text area

--- src/app/features/voice-app/pages/tts/tts.ts
+++ src/app/features/voice-app/pages/tts/tts.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { PageHeader } from "../../../../shared/components/page-header/page-header";
-import { TtsSettings } from "./components/tts-settings/tts-settings";
+import { Component, signal } from '@angular/core';
+import { PageHeader } from '../../../../shared/components/page-header/page-header';
+import { TtsSettings } from './components/tts-settings/tts-settings';
+import { HlmInputGroupImports } from '@spartan-ng/helm/input-group';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tts',
-  imports: [PageHeader, TtsSettings],
+  imports: [PageHeader, TtsSettings, HlmInputGroupImports, FormsModule],
   templateUrl: './tts.html',
   styleUrl: './tts.css',
 })
 export class Tts {
+  text = signal<string>('');
+  textareaMaxLength = signal<number>(2000);
 
+  clearText = () => {
+    this.text.set('');
+  };
+
+  setDemoText = () => {};
 }
